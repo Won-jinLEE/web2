99371a862a5c8cff95e99eab6bee3169171cfe3c
Won-jinLEE <127727068+Won-jinLEE@users.noreply.github.com>
2023-03-16 18:15:43 +0900

    Add files via upload

---
+++ 03.10.sql
@@ -0,0 +1,35 @@
+-- INSERT INTO student2
+-- (title, content)
+-- VALUES
+-- ('제목1','내용1'),
+-- ('제목2','내용2'),
+-- ('제목3','내용3'),
+-- ('제목4','내용4'),
+-- ('제목5','내용5');
+
+-- SELECT * FROM student2;
+
+-- UPDATE student2 SET title = "제목20", content = "내용20"
+-- WHERE NO = 20;
+
+-- DELETE FROM student2 WHERE title='제목32';
+
+-- SELECT *
+-- FROM student2
+-- WHERE NO = 32 OR NO = 34;
+
+-- ALTER TABLE `student2` AUTO_INCREMENT = 1;
+
+-- SELECT * FROM student2 WHERE NO='20';
+-- SELECT * FROM student2;
+
+-- INSERT INTO student2
+-- (title, content)
+-- VALUES
+-- ('제목21', '내용21'),
+-- ('제목22', '내용22'),
+-- ('제목23', '내용23'),
+-- ('제목24', '내용24'),
+-- ('제목25', '내용25');
+
+
